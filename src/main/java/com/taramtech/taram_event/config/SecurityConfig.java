f0f3ab839876419ad21cd3070b5d861eaf6c4be7
CHAIBOU MAKADA MADO Djibril <djibrilchaibou.dc@gmail.com>
2025-09-15 10:14:24 +0100

    Mise à jour de la configuration de sécurité et ajout de l'utilisateur dans les modèles JTE

--- src/main/java/com/taramtech/taram_event/config/SecurityConfig.java
+++ src/main/java/com/taramtech/taram_event/config/SecurityConfig.java
@@ -15,41 +15,44 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/css/**", "/assets/**", "/js/**", "/images/**")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.GET, "/", "/events/**")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST, "/", "/events/**")
-                        .hasAnyRole("ORGANIZER", "ADMIN")
-                        .requestMatchers("/login", "register")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorize -> authorize
+                                                .requestMatchers(HttpMethod.GET, "/css/**", "/assets/**", "/js/**",
+                                                                "/images/**")
+                                                .permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/events/**")
+                                                .permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/events/{id}/edit", "/events/create")
+                                                .hasAnyRole("ORGANIZER", "ADMIN")
+                                                .requestMatchers(HttpMethod.POST, "/events/**")
+                                                .hasAnyRole("ORGANIZER", "ADMIN")
+                                                .requestMatchers("/login", "register")
+                                                .permitAll()
+                                                .anyRequest()
+                                                .authenticated())
 
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/dashboard", true)
-                        .failureUrl("/login?error=true")
-                        .permitAll())
+                                .formLogin(form -> form
+                                                .loginPage("/login")
+                                                .loginProcessingUrl("/login")
+                                                .defaultSuccessUrl("/dashboard", true)
+                                                .failureUrl("/login?error=true")
+                                                .permitAll())
 
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll())
-                .csrf(csrf -> csrf.disable());
+                                .logout(logout -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/login?logout=true")
+                                                .invalidateHttpSession(true)
+                                                .deleteCookies("JSESSIONID")
+                                                .permitAll())
+                                .csrf(csrf -> csrf.disable());
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
